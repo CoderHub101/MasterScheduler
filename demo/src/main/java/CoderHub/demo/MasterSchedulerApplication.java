af7c252d3b5b19529b7568eaa487b4a33a1093a0
0zzy4 <josborne43@gatech.edu>
2024-05-21 21:29:35 -0400

    spring initializer

---
+++ demo/src/main/java/CoderHub/demo/MasterSchedulerApplication.java
@@ -0,0 +1,13 @@
+package CoderHub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterSchedulerApplication.class, args);
+	}
+
+}
